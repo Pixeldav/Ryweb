8aa8874326ab2cfadba8b8500fdff81cbf8b2fca
pickzel <93448366+Pixeldav@users.noreply.github.com>
2025-11-07 13:24:49 -0600

    more tenna + button

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
     // script.js
-    console.log("Welcome to your Netlify site!");
+    const tennaPopup = document.querySelector('#tennaPopup')
+    const tennaTextbox = document.querySelector('#tennaTextbox')
+    const btn1 = document.querySelector('#btn1')
+
+    btn1.addEventListener('click',()=>{
+        btn1.style.display = "none";
+        setTimeout(() => {
+        tennaPopup.src = "images/tennaStandWobble.gif"
+        tennaTextbox.src = "images/tennaTextbox1.gif"
+        }, 1500);
+    })
